1e39b8c4a1f6b6036d0ac214a86e52d14e9ea4bd
akakou <s16003@tokyo.kosen-ac.jp>
2017-10-12 01:38:58 +0900

    [update] インデントの高さを変更＆一部タブをスペースにした

--- nabeatsu.s
+++ nabeatsu.s
@@ -7,36 +7,36 @@ section .data
 
 ; コードセクション
 section .text
-        global _start           ; _startを指名
+    global _start               ; _startを指名
 
 
 ; スタート
-_start:                          ; メインのポイント
-        ; 必要な値の代入
-        push  rbp                ; スタックフレームのセット
-        mov   rdx, 1            ; カウントの初期化
+_start:                         ; メインのポイント
+    ; 必要な値の代入
+    push  rbp                   ; スタックフレームのセット
+    mov   rdx, 1                ; カウントの初期化
 
-        ; 100回ループする
-        mov rcx, 100            ; ループ回数の指定
-        _loop:
-            ; レジスタの値をスタックに移動
-            push  rcx           ; rcxをスタックに置く
-            push  rdx           ; rdxをスタックに置く
-            ; printfに使うレジスタの値を指定のレジスタに移動
-            mov    rdi, fmt      ; printfのフォーマットをレジスタに置く
-            mov    rsi, rdx      ; 表示する値を指定のレジスタに置く
-            mov    rax, 0        ; わからない → 消すとセグフォする
+    ; 100回ループする
+    mov rcx, 100                ; ループ回数の指定
+    _loop:
+        ; レジスタの値をスタックに移動
+        push  rcx               ; rcxをスタックに置く
+        push  rdx               ; rdxをスタックに置く
+        ; printfに使うレジスタの値を指定のレジスタに移動
+        mov    rdi, fmt         ; printfのフォーマットをレジスタに置く
+        mov    rsi, rdx         ; 表示する値を指定のレジスタに置く
+        mov    rax, 0           ; わからない → 消すとセグフォする
 
-            call  printf        ; printfを呼ぶ
-            ; スタックの値をレジスタに戻す
-            pop   rdx           ; rdxをスタックから持ってくる
-            pop   rcx           ; rcxをスタックから持ってくる
-            add   rdx, 1        ; rdxに１加算
+        call  printf            ; printfを呼ぶ
+        ; スタックの値をレジスタに戻す
+        pop   rdx               ; rdxをスタックから持ってくる
+        pop   rcx               ; rcxをスタックから持ってくる
+        add   rdx, 1            ; rdxに１加算
 
-            loop _loop          ; ループバック
+        loop _loop              ; ループバック
 
-        pop   rbp                ; スタックを戻す
+    pop   rbp                   ; スタックを戻す
 
-        mov   rax, 1            ; sys_exit
-        mov   rbx, 0            ; 終了ステータスコード
-        int   0x80              ; システムコール
+    mov   rax, 1                ; sys_exit
+    mov   rbx, 0                ; 終了ステータスコード
+    int   0x80                  ; システムコール
