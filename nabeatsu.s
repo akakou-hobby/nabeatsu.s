4f47bdaebc84b5edf56049f14ee9fcde10516e48
akakou <s16003@tokyo.kosen-ac.jp>
2017-10-13 23:38:52 +0900

    [update] セクションに分けた

--- nabeatsu.s
+++ nabeatsu.s
@@ -18,23 +18,27 @@ _start:                         ; メインのポイント
 
     ; 100回ループする
     mov rcx, 100                ; ループ回数の指定
-    _loop:
-        ; レジスタの値をスタックに移動
-        push  rcx               ; rcxをスタックに置く
-        push  rdx               ; rdxをスタックに置く
-        ; printfに使うレジスタの値を指定のレジスタに移動
-        mov    rdi, fmt         ; printfのフォーマットをレジスタに置く
-        mov    rsi, rdx         ; 表示する値を指定のレジスタに置く
-        mov    rax, 0           ; わからない → 消すとセグフォする
 
-        call  printf            ; printfを呼ぶ
-        ; スタックの値をレジスタに戻す
-        pop   rdx               ; rdxをスタックから持ってくる
-        pop   rcx               ; rcxをスタックから持ってくる
-        add   rdx, 1            ; rdxに１加算
 
-        loop _loop              ; ループバック
+_loop:
+    ; レジスタの値をスタックに移動
+    push  rcx               ; rcxをスタックに置く
+    push  rdx               ; rdxをスタックに置く
+    ; printfに使うレジスタの値を指定のレジスタに移動
+    mov    rdi, fmt         ; printfのフォーマットをレジスタに置く
+    mov    rsi, rdx         ; 表示する値を指定のレジスタに置く
+    mov    rax, 0           ; わからない → 消すとセグフォする
 
+    call  printf            ; printfを呼ぶ
+    ; スタックの値をレジスタに戻す
+    pop   rdx               ; rdxをスタックから持ってくる
+    pop   rcx               ; rcxをスタックから持ってくる
+    add   rdx, 1            ; rdxに１加算
+
+    loop _loop              ; ループバック
+
+
+_end:
     ; 後処理
     pop   rbp                   ; スタックを戻す
 
